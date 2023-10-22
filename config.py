5a243e972b025347b0469ffb8cc33b6430b70b7f
Espro Music <143268188+EsproMusic@users.noreply.github.com>
2023-10-22 19:20:30 +0530

    Espro Music

--- config.py
+++ config.py
@@ -81,18 +81,18 @@ START_IMG_URL = getenv(
     "START_IMG_URL", "https://te.legra.ph/file/1d5879f88f80e269ca8ca.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://te.legra.ph/file/b8a0c1a00db3e57522b53.jpg"
+    "PING_IMG_URL", "https://graph.org/file/b55f7f3380b612fc84574.jpg"
 )
-PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
-STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
-TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
-SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
-YOUTUBE_IMG_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
+PLAYLIST_IMG_URL = "https://graph.org/file/b55f7f3380b612fc84574.jpg"
+STATS_IMG_URL = "https://graph.org/file/b55f7f3380b612fc84574.jpg"
+TELEGRAM_AUDIO_URL = "https://graph.org/file/b55f7f3380b612fc84574.jpg"
+TELEGRAM_VIDEO_URL = "https://graph.org/file/b55f7f3380b612fc84574.jpg"
+STREAM_IMG_URL = "https://graph.org/file/b55f7f3380b612fc84574.jpg"
+SOUNCLOUD_IMG_URL = "https://graph.org/file/b55f7f3380b612fc84574.jpg"
+YOUTUBE_IMG_URL = "https://graph.org/file/b55f7f3380b612fc84574.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://graph.org/file/b55f7f3380b612fc84574.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://graph.org/file/b55f7f3380b612fc84574.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://graph.org/file/b55f7f3380b612fc84574.jpg"
 
 
 def time_to_seconds(time):
