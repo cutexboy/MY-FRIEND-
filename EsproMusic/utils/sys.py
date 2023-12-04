9947268c5467ca5afe5826a1060b14c5d6d9b3d9
Espro Music <143268188+EsproMusic@users.noreply.github.com>
2023-12-04 22:24:59 +0530

    Espro Music

--- EsproMusic/utils/sys.py
+++ EsproMusic/utils/sys.py
@@ -8,6 +8,9 @@ from EsproMusic.utils.formatters import get_readable_time
 
 async def bot_sys_stats():
     bot_uptime = int(time.time() - _boot_)
+    cpu = psutil.cpu_percent(interval=0.5)
+    mem = psutil.virtual_memory().percent
+    disk = psutil.disk_usage("/").percent
     UP = f"{get_readable_time(bot_uptime)}"
     CPU = f"{psutil.cpu_percent(interval=0.5)}%"
     RAM = f"{psutil.virtual_memory().percent}%"
