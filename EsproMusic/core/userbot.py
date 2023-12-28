bb84a0d157ca11cea4cd24fb610c922fc44aaeda
Prince kumar <112043341+cutexboy@users.noreply.github.com>
2023-12-28 08:24:51 +0530

    Baby Update userbot.py

--- EsproMusic/core/userbot.py
+++ EsproMusic/core/userbot.py
@@ -53,8 +53,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("fabulousefried")
-                await self.one.join_chat("the_music_update")
+                await self.one.join_chat("FRIENDS2FAMILY_0")
+                await self.one.join_chat("The_F2F_Shayri")
             except:
                 pass
             assistants.append(1)
@@ -74,8 +74,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("fabulousefried")
-                await self.one.join_chat("the_music_update")
+                await self.two.join_chat("FRIENDS2FAMILY_0")
+                await self.one.join_chat("The_F2F_Shayri")
             except:
                 pass
             assistants.append(2)
@@ -95,8 +95,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("fabulousefried")
-                await self.one.join_chat("the_music_update")
+                await self.three.join_chat("FRIENDS2FAMILY_0")
+                await self.one.join_chat("The_F2F_Shayri")
             except:
                 pass
             assistants.append(3)
@@ -116,8 +116,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("fabulousefried")
-                await self.one.join_chat("the_music_update")
+                await self.four.join_chat("FRIENDS2FAMILY_0")
+                await self.one.join_chat("The_F2F_Shayri")
             except:
                 pass
             assistants.append(4)
@@ -137,8 +137,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("fabulousefried")
-                await self.one.join_chat("the_music_update")
+                await self.five.join_chat("FRIENDS2FAMILY_0")
+                await self.one.join_chat("The_F2F_Shayri")
             except:
                 pass
             assistants.append(5)
